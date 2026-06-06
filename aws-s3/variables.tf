d4886031784c9fa496e94419ed9d0b31b7eabf88
bodax <markpolo525@gmail.com>
2026-06-06 13:49:16 +0200

    Add variables file

--- aws-s3/variables.tf
+++ aws-s3/variables.tf
@@ -1,9 +1,11 @@
 variable "bucket_name" {
   type        = string
   description = "Name of the S3 bucket"
+  default     = "cmtr-031bfa7b-bucket-1780745438"
 }
 
 variable "project_tag" {
   type        = string
   description = "Project tag value used for resource tagging"
+  default     = "cmtr-031bfa7b"
 }
