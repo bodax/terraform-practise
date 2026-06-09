ce7be6fb8f77c35a5e85ae16e58cd5636b89ee6b
bodax <markpolo525@gmail.com>
2026-06-09 18:54:29 +0200

    Fix

--- aws-trf-modules/variables.tf
+++ aws-trf-modules/variables.tf
@@ -1,8 +1,10 @@
 variable "region" {
-  type    = string
-  default = "eu-west-1"
+  type        = string
+  description = "AWS region to deploy resources into"
+  default     = "eu-west-1"
 }
 
 variable "allowed_ip_range" {
-  type = list(string)
+  type        = list(string)
+  description = "List of CIDR blocks allowed to access SSH and HTTP endpoints"
 }
